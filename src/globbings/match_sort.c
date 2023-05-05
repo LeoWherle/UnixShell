986f21eabba3a5af42a357f97929441e988ef6d5
mathematisse <lucien.pineau@epitech.eu>
2023-05-05 16:26:12 +0200

    [FIX] Sorting order for upper chars

--- src/globbings/match_sort.c
+++ src/globbings/match_sort.c
@@ -9,8 +9,31 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <ctype.h>
 #include "mystr.h"
 #include "globber.h"
+
+static int should_switch(char *str1, char *str2)
+{
+    int i = 0;
+
+    if (!str1 || !str2) return 0;
+    str1 = strdup(str1);
+    ASSERT_PTR(str1, 0);
+    str2 = strdup(str2);
+    if (!str2) {
+        free(str1);
+        return 0;
+    }
+    for (; str1[i]; i++)
+        str1[i] = tolower(str1[i]);
+    for (i = 0; str2[i]; i++)
+        str2[i] = tolower(str2[i]);
+    i = strcmp(str1, str2);
+    free(str1);
+    free(str2);
+    return i > 0;
+}
 
 static void sort_step(node_t *i)
 {
@@ -18,7 +41,7 @@ static void sort_step(node_t *i)
     node_t *j = NULL;
 
     for (j = i->next; j; j = j->next) {
-        if (strcmp(i->data, j->data) > 0) {
+        if (should_switch(i->data, j->data)) {
             tmp = i->data;
             i->data = j->data;
             j->data = tmp;
